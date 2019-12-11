7422e02a70e56aa0dd05e7d6d8211d71c765e9c9
Kyle Calica <stcalica@users.noreply.github.com>
2019-12-11 08:59:33 -0800

    Update getImageSizes.py

--- getImageSizes.py
+++ getImageSizes.py
@@ -14,7 +14,7 @@ import json
 
 def get_urls(page):
     url = "http://staging.clearcom.com/wp-json/wp/v2/media"
-    querystring = {"page": page,"per_page": 100,"key":"AIzaSyCnSv6GsxFnfBPNwk57CfNQFj7IrTephZs","orderby":"id","order":"asc"}
+    querystring = {"page": page,"per_page": 100,"key":"XXXXX","orderby":"id","order":"asc"}
     response = requests.request("GET", url, params=querystring)
     total_pages = response.headers['X-WP-TotalPages']
     attachments = []
@@ -99,30 +99,6 @@ def main():
                 else:
                     x += 1
                     num_of_media -= 1
-
-
-# last page was 21
-# last id was 5215
-
-    # credentials = ServiceAccountCredentials.from_json_keyfile_name("spreadsheet-api-clearcom.json", scope)
-    # gc = gspread.authorize(credentials)
-    # sheet = gc.open_by_key("1N-sLTnNMyCl5ZQyFHrKU6j09Cd2r4EplewcFnkG5O8U").sheet1
-    # urls = sheet.col_values(3)
-
-    # with open("imageSizes.csv", "w") as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(["url","size"])
-    #     for url in urls:
-    #         response = requests.get(url)
-    #         if (response.status_code == 200) and ("Content-Length" in response.headers):
-    #             #convert each size into kb or mb or gb
-    #             bytes = humanfriendly.format_size(int(response.headers["Content-Length"]))
-    #             print(url + ", " +  bytes)
-    #             writer.writerow([url, bytes])
-    #         else:
-    #             print(url + " did not get successfully")
-    #             print(response.headers)
-    # return
 
 if "__main__" == __name__:
     main()
